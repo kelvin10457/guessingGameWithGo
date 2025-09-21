1ab941277912ed6fd1410707b9b1532675debab9
kelvin10457 <122730716+kelvin10457@users.noreply.github.com>
2025-09-21 12:07:13 -0500

    lil proyect just finished

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"log"
 	"math/rand"
@@ -15,32 +16,41 @@ var reader *bufio.Reader = bufio.NewReader(os.Stdin)
 func getInput() (int, error) {
 	valueEntered, err := reader.ReadString('\n')
 	if err != nil {
-		return -1, err
+		return -1, errors.New("there was an error reading the input")
 	}
-	valueEntered = strings.Replace(valueEntered, "\n", "", 1)
+	valueEntered = strings.TrimSpace(valueEntered)
+
 	intValueEntered, err := strconv.Atoi(valueEntered)
 	if err != nil {
-		return -1, err
+		return -1, errors.New("you should enter a number")
 	}
 	return intValueEntered, nil
 }
 
 func guess(chances int) {
+	fmt.Printf("You have %d chances to guess the correct number.\n", chances)
 	randomNumber := rand.Intn(100)
+	isGuessed := false
 	for i := 0; i < chances; i++ {
 		fmt.Print("Enter your choice: ")
 		numberGuess, err := getInput()
-		fmt.Println()
-		if err != nil {
 
+		if err != nil {
+			log.Fatal(err)
 		}
+
 		if numberGuess == randomNumber {
+			isGuessed = true
 			fmt.Printf("Congratulations! You guessed the correct number in %d attempts. \n", (i + 1))
+			break
 		} else if numberGuess < randomNumber {
 			fmt.Printf("Incorrect! The number is greater than %d. \n", numberGuess)
 		} else {
 			fmt.Printf("Incorrect! The number is less than %d. \n", numberGuess)
 		}
+	}
+	if !isGuessed {
+		fmt.Println("Game Over. Try again.")
 	}
 }
 func main() {
@@ -56,7 +66,7 @@ func main() {
 	fmt.Print("->")
 	level, err := getInput()
 	if err != nil {
-		log.Fatal("Hubo un error al seleccionar el nivel")
+		log.Fatal(err)
 	}
 	switch level {
 	case 1:
